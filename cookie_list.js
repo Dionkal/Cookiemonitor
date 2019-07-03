fba5cf0bff29a2ec62eb5276852b38871d774958
Dennis Kalochristianakis <dionyshs97@hotmail.com>
2019-07-03 17:08:01 +0300

    Implemented reading cookies with the list-cookies example

--- cookie_list.js
+++ cookie_list.js
@@ -1,4 +1,35 @@
-// let tab = tabs.pop();
-// var cookieList = document.cookie;
-var cookieList = browser.cookies.getALL();
+function CookieCallback(tabs) {
+	let tab = tabs.pop();
+	var gettingAllCookies = browser.cookies.getAll({ url: tab.url });
+	gettingAllCookies.then((cookies) => {
 
+		//set the header of the panel
+		var activeTabUrl = document.getElementById('header-title');
+		var text = document.createTextNode("Cookies at: " + tab.title);
+		var cookieList = document.getElementById('cookie-list');
+		activeTabUrl.appendChild(text);
+
+		if (cookies.length > 0) {
+			for (let cookie of cookies) {
+				let li = document.createElement("li");
+				let content = document.createTextNode(cookie.name + ": " + cookie.value);
+				li.appendChild(content);
+				cookieList.appendChild(li);
+			}
+		} else {
+			let p = document.createElement("p");
+			let content = document.createTextNode("No cookies in this tab.");
+			let parent = cookieList.parentNode;
+
+			p.appendChild(content);
+			parent.appendChild(p);
+		}
+	});
+}
+
+function getBrowserTab() {
+	return browser.tabs.query({ currentWindow: true, active: true });
+}
+
+
+getBrowserTab().then(CookieCallback);

eee95ce274131ea23574c2e0d0bcd18cc7c360f9
Dennis_Kalo <dionkal@protonmail.com>
2020-02-27 21:30:37 +0200

    Decoded consent string

---
+++ decodeConsentString.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { ConsentString } = require('consent-string');
+
+const consentData = new ConsentString('BOvcPVsOvcPVsAKAACENC_wAAAAuhr_7f_52_8_u3fZ_NuinOp_j_ef_XWUcLZYvcAvzhY9Zfi_EwhU4m_0PRM9ycgx85cpDCsoxQ7CSkCCBGgNeTtn9mzgWxoRP6wEcJrz33bEw-ro2v-ZzACGN_YhEyA');
+console.log(consentData);
+
+console.log("Device Access Conset: ", consentData.getPurposesAllowed(1));
+console.log("Personalize Advertizing Conset: ", consentData.getPurposesAllowed(1));

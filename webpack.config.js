d8dda928e6fc8df9dae63734f478829c80afa247
Dennis_Kalo <dionkal@protonmail.com>
2020-05-20 20:52:57 +0300

    Temporary changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/decodeConsentString.js',
+  entry: [
+    './src/decodeConsentString.js',
+    './src/cookie_list.js'
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
